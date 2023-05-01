905e5874e6434e1cc2022b9a80e72ba8a8a7a116
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-01 17:04:22 -0400

    Edited globales variables and resolution, exercise00.py

---
+++ exercise01.py
@@ -0,0 +1,13 @@
+# Globales variables
+cal_1 = (1 + 2)**3
+cal_2 = "DA" * 4
+cal_4 = ("PA" + "LA") * 2
+cal_6 = 5 / 2
+cal_7 = 5 // 2
+cal_8 = 5 % 2
+
+# Resolution
+result = cal_1, cal_2, cal_4, cal_6, cal_7, cal_8
+
+# Print out result
+print(result)
