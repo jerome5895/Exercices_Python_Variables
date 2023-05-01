64c626d8d02b7d8ccd75de0e567b4508f1b6afa9
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-01 00:14:45 -0400

    Completed function, exercise00.py

--- exercise00.py
+++ exercise00.py
@@ -1,8 +1,7 @@
-result = first_cal
-if 736 in result:
-    print("nombres de Friedman")
-
-
+# Function to know if it's a number of Friedman
+def if_friedman(cal):
+    if 736 in cal:
+        print("C'est un nombre de Friedman")
 
 # Globales variables
 first_cal = 7 + 3**6
@@ -10,6 +9,8 @@ second_cal = (3 + 4)**3
 third_cal = 3**6 - 5
 fourth_cal = (1 + 2**8) * 5
 fifth_cal = (2 + 1**8)**7
+
+# Resolution
 
 # Print out result
 print(result)
