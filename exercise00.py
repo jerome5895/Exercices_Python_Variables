a2b5ea62c2ad6099f3894090edc9084663b556b9
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-01 00:11:42 -0400

    Completed globales variables, exercise00.py

---
+++ exercise00.py
@@ -0,0 +1,15 @@
+result = first_cal
+if 736 in result:
+    print("nombres de Friedman")
+
+
+
+# Globales variables
+first_cal = 7 + 3**6
+second_cal = (3 + 4)**3
+third_cal = 3**6 - 5
+fourth_cal = (1 + 2**8) * 5
+fifth_cal = (2 + 1**8)**7
+
+# Print out result
+print(result)
