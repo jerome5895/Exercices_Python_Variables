eae6eccae6f633c8fed0e576e47e0c857d8f2717
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-01 16:43:35 -0400

    Finished resolution, exercise00.py

--- exercise00.py
+++ exercise00.py
@@ -1,7 +1,7 @@
 # Function to know if it's a number of Friedman
-def if_friedman(cal_1, cal_2, cal_3, cal_4, cal_5):
-    if cal_1 or cal_2 or cal_3 or cal_4 or cal_5 == 736 or 343 or 724 or 1285 or 2187:
-        print(f"{cal} Est un nombre de Friedman")
+def if_friedman(cal):
+    if cal == 736 or 343 or 724 or 1285 or 2187:
+        return (f"{cal} Est un nombre de Friedman")
 
 # Globales variables
 first_cal = 7 + 3**6
@@ -11,10 +11,11 @@ fourth_cal = (1 + 2**8) * 5
 fifth_cal = (2 + 1**8)**7
 
 # Resolution
-if_friedman(first_cal)
-if_friedman(second_cal)
-if_friedman(third_cal)
-if_friedman(fourth_cal)
-if_friedman(fifth_cal)
+result_1 = if_friedman(first_cal)
+result_2 = if_friedman(second_cal)
+result_3 = if_friedman(third_cal)
+result_4 = if_friedman(fourth_cal)
+result_5 = if_friedman(fifth_cal)
 
 # Print out result
+print((result_1, result_2, result_3, result_4, result_5), sep="/t ")
