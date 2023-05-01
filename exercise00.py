7096a5555aa0633dc649dfa7c62263dcf483e104
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-01 01:01:53 -0400

    Deleted 4 functions, edited called functions, exercise00.py

--- exercise00.py
+++ exercise00.py
@@ -1,24 +1,7 @@
 # Function to know if it's a number of Friedman
 def if_friedman(cal):
-    if cal == 736:
+    if cal == 736 or 343 or 365 or 1285 or 2187:
         print(f"{cal} Est un nombre de Friedman")
-
-def if_friedman(cal):
-    if cal == 736:
-        print(f"{cal} Est un nombre de Friedman")
-
-def if_friedman(cal):
-    if cal == 736:
-        print(f"{cal} Est un nombre de Friedman")
-
-def if_friedman(cal):
-    if cal == 736:
-        print(f"{cal} Est un nombre de Friedman")
-
-def if_friedman(cal):
-    if cal == 736:
-        print(f"{cal} Est un nombre de Friedman")
-
 
 # Globales variables
 first_cal = 7 + 3**6
@@ -29,5 +12,9 @@ fifth_cal = (2 + 1**8)**7
 
 # Resolution
 if_friedman(first_cal)
+if_friedman(second_cal)
+if_friedman(third_cal)
+if_friedman(fourth_cal)
+if_friedman(fifth_cal)
 
 # Print out result
